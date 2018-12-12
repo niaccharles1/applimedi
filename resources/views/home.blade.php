4cf128eb0a11054b81080d9033d616c25e541c5c
charles <niaccharles2@gmail.com>
2018-12-12 13:52:50 +0000

    Procfile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
-                        <marquee> </b><i>SAMUEL LE PROPHETE</i></b></marquee>
+                        
 
                             {{ session('status') }}
                         </div>
